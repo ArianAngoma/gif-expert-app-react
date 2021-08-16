3d03100c6c811ed0289e038c95b159b691c8e7b9
unknown <arian.angoma@tecsup.edu.pe>
2021-08-16 17:30:06 -0500

    Button Add element to array and State

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,8 +1,26 @@
+import {useState} from "react";
+
 const GifExpertApp = () => {
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleApp = () => {
+        // setCategories([...categories, 'Anime']);
+        setCategories(cats => [...cats, 'Anime']);
+    }
+
     return (
         <div>
             <h2>GifExpertApp</h2>
             <hr/>
+
+            <button onClick={handleApp}>Agregar</button>
+
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })}
+            </ol>
         </div>
     )
 }
