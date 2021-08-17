08fea8a550bf2087777e3e7e2c07440af7c1626a
unknown <arian.angoma@tecsup.edu.pe>
2021-08-16 19:11:38 -0500

    Get Img and render it to component GifGrid

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,9 +1,10 @@
 import {useState} from "react";
 import AddCategory from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
 
 const GifExpertApp = () => {
     // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
-    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+    const [categories, setCategories] = useState(['One Punch']);
 
     // Agregar elemento al array con useState
     /*const handleApp = () => {
@@ -19,9 +20,14 @@ const GifExpertApp = () => {
             <hr/>
 
             <ol>
-                {categories.map(category => {
-                    return <li key={category}>{category}</li>
-                })}
+                {
+                    categories.map(category => (
+                        <GifGrid
+                            key={category}
+                            category={category}
+                        />
+                    ))
+                }
             </ol>
         </div>
     )
