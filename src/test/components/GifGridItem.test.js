e512751b6128e0a5f67de9da79974299011f9114
unknown <arian.angoma@tecsup.edu.pe>
2021-08-18 19:19:14 -0500

    Test GifGridItem

--- src/test/components/GifGridItem.test.js
+++ src/test/components/GifGridItem.test.js
@@ -9,9 +9,27 @@ describe('Test to component <GifGridItem/>', () => {
 
     beforeEach(() => {
         wrapper = shallow(<GifGridItem title={title} url={url}/>);
-    })
+    });
 
     test('Should display the component <GifGridItem/> correctly', () => {
         expect(wrapper).toMatchSnapshot();
     });
+
+    test('Should have a paragraph with the title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    });
+
+    test('Should have the image equal to the url and alt', () => {
+        const img = wrapper.find('img');
+        // console.log(img.props().src);
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+
+    test('Should have animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        expect(className.includes('animate__fadeIn')).toBe(true);
+    })
 })
