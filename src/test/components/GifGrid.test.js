a5844711097f3b63ec2c17da199291de131c6306
unknown <arian.angoma@tecsup.edu.pe>
2021-08-19 18:53:36 -0500

    Test component GifGrid

--- src/test/components/GifGrid.test.js
+++ src/test/components/GifGrid.test.js
@@ -1,11 +1,11 @@
 import {shallow} from "enzyme";
 import GifGrid from "../../components/GifGrid";
 import {useFetchGifs} from "../../hooks/useFetchGifs";
+
 jest.mock('../../hooks/useFetchGifs');
 
 describe('Test to component <GifGrid/>', () => {
     const category = 'Anime'
-
 
     test('Should display the component <GifGrid/> correctly', () => {
         useFetchGifs.mockReturnValue({
@@ -17,11 +17,20 @@ describe('Test to component <GifGrid/>', () => {
     });
 
     test('should show items when loading images useFetchGifs', () => {
+        const gifs = [{
+            id: 'ayMW3eqvuP00o',
+            title: 'No Face GIF by Spirited Away',
+            url: 'https://giphy.com/gifs/spirited-away-no-face-ayMW3eqvuP00o'
+        }];
+
         useFetchGifs.mockReturnValue({
-            data: [{id: 'ayMW3eqvuP00o', title: 'No Face GIF by Spirited Away', url: 'https://giphy.com/gifs/spirited-away-no-face-ayMW3eqvuP00o'}],
+            data: gifs,
             loading: false
-        })
+        });
+
         const wrapper = shallow(<GifGrid category={category}/>);
         expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('p').exists()).toBe(false);
+        expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
     });
 })
