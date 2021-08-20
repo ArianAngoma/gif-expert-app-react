9c4c991c619b8240d1d90f207eab77155b86a3d7
unknown <arian.angoma@tecsup.edu.pe>
2021-08-19 19:25:37 -0500

    Test GifGrid

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -2,12 +2,22 @@ import {renderHook} from "@testing-library/react-hooks";
 import {useFetchGifs} from "../../hooks/useFetchGifs";
 
 describe('tests on the hook useFetchGifs', () => {
-    test('Should return the initial state', () => {
-        const {result} = renderHook(() => useFetchGifs('One Punch'));
+    test('Should return the initial state', async () => {
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('One Punch'));
         const {data, loading} = result.current;
+        await waitForNextUpdate();
         console.log({data, loading});
 
         expect(data).toEqual([]);
         expect(loading).toBe(true);
+    });
+
+    test('Should return an array of images and loading in false', async () => {
+        const {result, waitForNextUpdate} = renderHook(() => useFetchGifs('One Punch'));
+        await waitForNextUpdate();
+        const {data, loading} = result.current;
+
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
     })
 });
